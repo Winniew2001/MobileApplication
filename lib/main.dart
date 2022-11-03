c494f73612536b6520bf87f3058df5564f0d9df9
Winnie <CuteDragon246@gmail.com>
2022-11-03 23:53:04 +0100

    Started adding firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFire
+  );
+
+
   runApp(const MyApp());
 }
 
