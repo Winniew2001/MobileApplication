5a3831d7be24fbce7e9fdf2274dbfd6afe03a4fd
Aron Selven <arons@Arons-MacBook-Pro.local>
2022-11-04 01:48:16 +0100

    Firebase dependencies mostly ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_application/firebase_options.dart';
 
 
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFire
+    options: DefaultFirebaseOptions.currentPlatform
   );
 
 
